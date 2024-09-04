dc92da67b0d410cde957ed9977d82cdda4faedf4
Hansanghyeon <999@hyeon.pro>
2024-09-04 03:39:20 +0000

    feat: eslint-plugin-arca 제거

--- src/plus.mjs
+++ src/plus.mjs
@@ -1,12 +1,2 @@
-import arca from 'eslint-plugin-arca';
-
 export default [
-  {
-    plugins: {
-      arca,
-    },
-    rules: {
-      'arca/import-align': ['error', { collapseExtraSpaces: true }],
-    }
-  }
 ]
